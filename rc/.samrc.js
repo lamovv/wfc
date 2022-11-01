4962c6c0e10fb8e26f0cc5e4fea27cc8dcce6750
lamovv <lamovv@gmail.com>
2022-11-01 10:34:52 +0800

    fix(sam): rc

--- rc/.samrc.js
+++ rc/.samrc.js
@@ -6,15 +6,11 @@ module.exports = {
   // silent: false,
   logLevel: 'DEBUG',
   https,
-  hosts: [
-    'pre.my.domain.com',
-    'daily.my.domain.com',
-    'my.domain.com',
-  ],
-  proxy: {
-    port: 8800,
-  },
-  openPath: '',
-  webPort: 8001,
-  webRoot: './dist', // 预览编译后效果
+  hosts: ['pre.my.domain.com', 'daily.my.domain.com', 'my.domain.com'],
+  path: '/', // 默认打开路径
+  // proxy: { // 代理端口等指定
+  //   port: 8800,  // 默认8880
+  // },
+  // // 预览编译后效果，配置编译结果目录
+  // webRoot: './dist',
 };
