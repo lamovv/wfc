aa38c9cb48565a8794f7b182e5983b07fd8b0868
lamovv <lamovv@gmail.com>
2022-12-28 17:48:54 +0800

    feat(sam): update

--- rc/.samrc.js
+++ rc/.samrc.js
@@ -4,12 +4,12 @@ const https = env.HTTPS == 1;
 
 module.exports = {
   // silent: false,
-  logLevel: 'DEBUG',
+  // logLevel: 'TRACE',
   https,
   hosts: ['pre.my.domain.com', 'daily.my.domain.com', 'my.domain.com'],
   path: '/', // 默认打开路径
-  // proxy: { // 代理端口等指定
-  //   port: 8800,  // 默认8880
+  // proxy: { // 代理配置
+  //   port: 8800,  // 指定代理服务端口，默认8880
   // },
   // // 预览编译后效果，配置编译结果目录
   // webRoot: './dist',
